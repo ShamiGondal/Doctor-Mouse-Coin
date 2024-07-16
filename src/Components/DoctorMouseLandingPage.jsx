94f4a451f3aafc625d5e4c4447400cc9ab008a57
Ehtisham Ahmed Gondal <ehtishamahmedgondal@gmail.com>
2024-07-16 21:40:45 +0500

    updated desing

--- src/Components/DoctorMouseLandingPage.jsx
+++ src/Components/DoctorMouseLandingPage.jsx
@@ -2,7 +2,10 @@ import 'tailwindcss/tailwind.css';
 import '../App.css';
 import '../thirdPage.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTelegram, faYoutube, faRedditAlien, faLinkedin, faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTelegram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import dexScreenerIcon from '../assets/dexScreenerIcon.png'
+import solScanIcon from '../assets/solanaIcon.png'
+import solScanIconTop from '../assets/solScanIconTop.png'
 import BackgroundVideo from '../assets/Videos/BackgroundVideo.mp4';
 import Logo from '../assets/rmFrh2fKMNYODW4ac7Em5-transformed.png'; // Adjust the path to your logo file
 import QRCode from '../assets/QRCode.png'; // Adjust the path to your QR code image file
@@ -10,7 +13,9 @@ import QRCodeButtom from '../assets/QRCodeButtom.png'; // Adjust the path to you
 import BackgroundImagePage3 from '../assets/backgroundImagePage3.svg'
 import backgroundImage from '../assets/secondpageBgImage.svg'; // Ensure to provide the correct path
 import MousesIcon from '../assets/mousesIcons.png'; // Ensure to provide the correct path
-import characterImage5 from '../assets/character15.png'; // Ensure to provide the correct path
+import doctormouseCircularAnimation from '../assets/doctormousecoinpresenting (2).png'; // Ensure to provide the correct path
+import characterImage5 from '../assets/characterpng8.png'; // Ensure to provide the correct path
+import characterImage6 from '../assets/characterpng7.png'; // Ensure to provide the correct path
 import teteCoinLogo from '../assets/coinImage.png';
 import fourthpagebgImage from '../assets/fourthpagebgimage.svg'
 import fifthpagebgImage from '../assets/fifthpagebgimage.jpeg'
@@ -24,7 +29,7 @@ function DoctorMouseLandingPage() {
 
 
     const [certificatecCopied, setCertificateCopied] = useState(false);
-    const certificateCode = 'GAVXJIJDVOVFQXOGKSTEXV7XGNK5WDSBNGCHNUUKQ';
+    const certificateCode = 'EVS9EoZoxaYZnk8k7jCukfkgG8ZtiF​wVtDHRLV4gy8Uz ';
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(certificateCode).then(() => {
@@ -49,7 +54,7 @@ function DoctorMouseLandingPage() {
                     style={{ perspective: '500px' }}
                 >
                     <img
-                        src={characterImage5}
+                        src={doctormouseCircularAnimation}
                         alt="Coin Video"
                         className="w-full h-full object-contain transform shadow-lg animate-spin-slow bg-cool-500 rounded-full"
                         style={{
@@ -142,19 +147,25 @@ function DoctorMouseLandingPage() {
             <div className='h-32 w-full bg-gradient-to-r from-gray-800 to-black flex justify-between items-center p-6 z-50'>
                 <div className='flex space-x-4'>
                     <div className="flex items-center justify-center space-x-4">
-                        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
+                        <a href="https://x.com/DrMouseCoin?t=4LV24L3j1VWhgtepFaDB3A&s=09" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
                             <div className="bg-gradient-to-br from-blue-900 via-blue-500 to-blue-300 rounded-full p-2 md:p-5 lg:px-6 shadow-lg image-3d">
-                                <i className="fab fa-twitter text-white text-xl"></i>
+                                <i className="fa-brands fa-x-twitter text-xl text-white"></i>
                             </div>
                         </a>
-                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
+                        <a href="https://t.me/+Cv2pmIa9OI0wMDVh" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
                             <div className="bg-gradient-to-br from-blue-900 via-blue-500 to-blue-300 rounded-full p-2 md:p-5 lg:px-6 shadow-lg image-3d">
-                                <i className="fab fa-facebook text-white text-xl"></i>
+                                <i className="fa-brands fa-telegram text-xl text-white"></i>
                             </div>
                         </a>
-                        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
+                        <a href="https://dexscreener.com/solana/apgie2rbaezrlbo1yur8cpcav6zfuy6dzcy9yebtezfs" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
                             <div className="bg-gradient-to-br from-blue-900 via-blue-500 to-blue-300 rounded-full p-2 md:p-5 lg:px-6 shadow-lg image-3d">
-                                <i className="fab fa-instagram text-white text-xl"></i>
+                                <img src={dexScreenerIcon} className=' h-6 w-6' ></img>
+
+                            </div>
+                        </a>
+                        <a href="https://solscan.io/token/EVS9EoZoxaYZnk8k7jCukfkgG8ZtiFwVtDHRLV4gy8Uz" target="_blank" rel="noopener noreferrer" className="transform hover:scale-110 transition-transform duration-300">
+                            <div className="bg-gradient-to-br from-blue-900 via-blue-500 to-blue-300 rounded-full p-2 md:p-5 lg:px-6 shadow-lg image-3d">
+                                <img src={solScanIconTop} className='h-8 w-8 text-white'  ></img>
                             </div>
                         </a>
                     </div>
@@ -189,7 +200,7 @@ function DoctorMouseLandingPage() {
                     </div>
                     <div className="p-4 border border-white bg-black bg-opacity-60 z-20 mb-8">
                         <img src={teteCoinLogo} alt="DCOTOR COIN Logo" className="mx-auto mb-4 w-24 h-24" />
-                        <p className="text-white">
+                        <p className="text-white font-bungee">
                             Meet Dr Mouse, the game-changing meme coin designed to transform the
                             healthcare facility of transactions! Faster payments?Check.Unmatched
                             transparency? Check.Bulletproof security? Check. Dr Mouse is laeding
@@ -208,17 +219,16 @@ function DoctorMouseLandingPage() {
                     style={{ backgroundImage: `url(${BackgroundImagePage3})` }}
                 ></div>
 
-                <div className="relative w-full max-w-4xl z-10 p-4">
-                    <h1 className="text-3xl font-bold mt-8 z-10 font-pressStart text-center mb-10 text-yellow-500">COIN STAGE</h1>
+                <div className="relative w-full max-w-4xl z-20 p-4">
+                    <h1 className="text-4xl font-bold mt-8 z-20 font-pressStart text-center mb-10 text-yellow-500">COIN STAGE</h1>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-center relative z-20">
+                    <div className="grid grid-cols-1 gap-8 text-center relative z-20 md:grid-cols-2">
                         <div className="p-4 border border-white bg-black bg-opacity-60">
                             <h2 className="text-xl font-bold font-bungee text-yellow-500">STAGE 1</h2>
                             <h2 className="text-xl font-bold font-bungee text-yellow-500">Token Creation</h2>
                             <p>Dr Mouse was developed using cutting-edge blockchain technology, creating a secure,
                                 scalable, and reliable token. It's a robust digital asset designed to revolutionize
                                 healthcare transactions."</p>
-
                         </div>
                         <div className="p-4 border border-white bg-black bg-opacity-60">
                             <h2 className="text-xl font-bold font-bungee text-yellow-500">STAGE 2</h2>
@@ -226,9 +236,8 @@ function DoctorMouseLandingPage() {
                             <p>Our innovative marketing strategies, including social media campaigns,
                                 influencer partnerships, and viral content, keep Dr Mouse at the
                                 forefront of the meme coin revolution."</p>
-
                         </div>
-                        <div className="p-4 border border-white bg-black bg-opacity-60 ">
+                        <div className="p-4 border border-white bg-black bg-opacity-60 md:col-span-2 md:w-1/2 md:mx-auto">
                             <h2 className="text-xl font-bold font-bungee text-yellow-500">STAGE 3</h2>
                             <h2 className="text-xl font-bold font-bungee text-yellow-500">Healthcare Facility</h2>
                             <p>Dr Mouse transforms healthcare transactions with decentralized, transparent,
@@ -236,15 +245,16 @@ function DoctorMouseLandingPage() {
                                 accurate transactions for all."</p>
                         </div>
                     </div>
-                    <div className="absolute inset-0 flex items-center justify-center w-[100%] h-[100%] z-0">
+                    <div className="absolute inset-0 flex items-center justify-center w-full h-full z-0">
                         <img
-                            src={characterImage5}
+                            src={characterImage6}
                             alt="Character"
-                            className="w-80 h-full object-cover opacity-50 transform scale-125 mt-0"
+                            className="w-80 h-full object-cover opacity-80 transform scale-125 mt-32"
                         />
                     </div>
-
                 </div>
+
+
             </div>
 
 
@@ -270,8 +280,7 @@ function DoctorMouseLandingPage() {
                     <div className="bg-gradient-to-br from-gray-700 to-gray-900 p-5 rounded-lg shadow-md border-4 border-yellow-500 transform skew-y-3 hover:skew-y-0 transition-transform duration-300 ease-in-out h-52">
                         <h2 className="text-lg md:text-xl lg:text-2xl font-bold text-center text-yellow-400">0% TAX + FAIR LAUNCH</h2>
                         <p className="text-yellow-200 mt-3 text-sm md:text-base lg:text-lg text-center">
-                            NO TAX ON BUYING OR SELLING WITH A STEALTH LAUNCH VIA ORION TOOLS ON RAYDIUM TO DETER WHALES AND BOTS. COMMUNITY. PROFIT. REPEAT!
-                        </p>
+                            NO TAX ON BUYING OR SELLING                        </p>
                     </div>
 
                     <div className="bg-gradient-to-br from-gray-700 to-gray-900 p-5 rounded-lg shadow-md border-4 border-blue-500 transform skew-y-3 hover:skew-y-0 transition-transform duration-300 ease-in-out h-52">
@@ -282,11 +291,7 @@ function DoctorMouseLandingPage() {
                     </div>
                 </div>
 
-                <div className="relative z-10 text-center mt-10">
-                    <div className="inline-block bg-gray-200 p-3 rounded-lg border-4 border-orange-600 shadow-lg transform hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <p className="text-gray-900 text-sm md:text-base lg:text-lg">#TETE / CA:GAYVXJJDVOFQXQKD5C7X7VGHXK5WBSIDCDNVKUQ</p>
-                    </div>
-                </div>
+
             </div>
 
 
@@ -313,37 +318,34 @@ function DoctorMouseLandingPage() {
                         </button>
                     </div>
                 </div>
-
-                <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mt-10 mx-5 relative">
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-10 mt-10 mx-5 relative">
                     <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5 ">
                         <img src={mouseIcon} alt="Phantom Logo" className="w-12 h-12" />
-                        <p className="mt-2"><b className='text-xl'>1.</b> Create a Solana based wallet in Phantom or your choice of app.</p>
+                        <p className="mt-2">
+                            <b className='text-xl'>1.</b> Create a Solana based wallet in Phantom or your choice of app.
+                        </p>
                     </div>
 
-                    <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5  ">
+                    <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5">
                         <img src={solanaPng} alt="Solana Logo" className="w-12 h-12" />
                         <p className="mt-2">2. Buy Solana from a trusted CEX or DEX and transfer to wallet.</p>
                     </div>
 
                     <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5">
                         <img src={teteCoinLogo} alt="Coin Logo" className="w-12 h-12" />
-                        <p className="mt-2">3. Swap it all for some sweet, sweet $DOCTOR COIN on your favorite DEX!</p>
+                        <p className="mt-2">3. Click on Buy now and swap it all for some Dr mouse coin on your favorite DEXI</p>
                     </div>
 
-                    <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5">
-                        <p className="text-lg">🚀 HODL and grow bags like the alpha amphibian you are!</p>
+                    <div className="col-span-1 md:col-span-3 flex justify-center">
+                        <button className="buy-now-button">
+                            <img src={mouseIcon} alt="Mouse Icon" />
+                            <a href='https://raydium.io/swap/?outputMint=EVS9EoZoxaYZnk8k7jCukfkgG8ZtiFwVtDHRLV4gy8Uz&inputMint=sol' target="_blank" rel="noopener noreferrer" className='text-white font-bold'>
+                                Buy Now
+                            </a>
+                        </button>
                     </div>
-
-                    <div className="hexagon flex flex-col items-center justify-center text-center text-white p-5">
-                        <p className="text-lg">🐸 Get riches that smell like vanilla.</p>
-                    </div>
-
-                    <img
-                        src={mouseIcon}
-                        alt="Frog"
-                        className="hidden lg:block absolute right-0 top-1/2 transform -translate-y-1/2 w-40 h-40 mt-16 image-3d"
-                    />
                 </div>
+
 
                 <footer className="footer w-full mt-10 bg-gray-800 text-white py-10">
                     <div className=" mx-auto md:flex md:flex-row justify-between items-center">
@@ -351,27 +353,19 @@ function DoctorMouseLandingPage() {
                         {/* Social Media Icons */}
                         <div className="footer-icons flex flex-col items-center mt-5 space-y-4">
                             <div className="flex flex-row justify-center space-x-4 icon-3d">
-                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faTwitter} size="lg" />
+                                <a href="https://x.com/DrMouseCoin?t=4LV24L3j1VWhgtepFaDB3A&s=09" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faXTwitter} size="md" />
                                 </a>
-                                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faFacebook} size="lg" />
-                                </a>
-                                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faInstagram} size="lg" />
-                                </a>
-                                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faLinkedin} size="lg" />
-                                </a>
-                                <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
+                                <a href="https://t.me/+Cv2pmIa9OI0wMDVh" target="_blank" rel="noopener noreferrer">
                                     <FontAwesomeIcon icon={faTelegram} size="lg" />
                                 </a>
-                                <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faYoutube} size="lg" />
+                                <a href="https://solscan.io/token/EVS9EoZoxaYZnk8k7jCukfkgG8ZtiFwVtDHRLV4gy8Uz" target="_blank" rel="noopener noreferrer" >
+                                    <img src={solScanIcon} className='h-6 w-6'  ></img>
                                 </a>
-                                <a href="https://reddit.com" target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faRedditAlien} size="lg" />
+                                <a href="https://dexscreener.com/solana/apgie2rbaezrlbo1yur8cpcav6zfuy6dzcy9yebtezfs" target="_blank" rel="noopener noreferrer">
+                                    <img src={dexScreenerIcon} className=' h-6 w-6' ></img>
                                 </a>
+
                             </div>
                             <p className="text-sm text-3d">© COPYRIGHT DOCTOR COIN 2024</p>
                             <img src={QRCodeButtom} alt="QR Code" className="w-40 h-40 mx-auto mb-2  image-3d" />
